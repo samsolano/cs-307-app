a1fd7f0daf70ac023902dc9af963cd66a90cd2db
Sam Solano <70192267+samsolano@users.noreply.github.com>
2025-04-11 10:50:48 -0700

    find user by id

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -35,6 +35,19 @@ const users = {
     ]
   };
 
+  const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
+
   const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
